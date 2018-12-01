0823ec5c14c95fc143642336a5e6e64720207251
Laurent G. OSSBSS AMIS UCC <lgueroult@hotmail.com>
2018-12-01 20:19:27 +0100

    Update codata2018.jl

--- src/codata2018.jl
+++ src/codata2018.jl
@@ -10,18 +10,18 @@ import PhysicalConstants: @constant, @derived_constant
 @constant(a_0, "Bohr radius", 0.529_177_210_67e-10,
           BigFloat(52_917_721_067)/BigFloat(1_000_000_000_000_000_000_000), u"m",
           1.2e-20, BigFloat(12)/BigFloat(1_000_000_000_000_000_000_000), "CODATA 2018")
-#@constant(r_h, "Bare Hydrogen radius", 0.529_177_210_30e-10,
-#          BigFloat(52_917_721_03)/BigFloat(1_000_000_000_000_000_000_000), u"m",
-#          1.2e-20, BigFloat(12)/BigFloat(1_000_000_000_000_000_000_000), "CODATA 2018")
+@constant(r_0, "Bare Hydrogen radius", 0.529_177_210_30e-10,
+          BigFloat(52_917_721_03)/BigFloat(1_000_000_000_000_000_000_000), u"m",
+          1.2e-20, BigFloat(12)/BigFloat(1_000_000_000_000_000_000_000), "CODATA 2018")
 @constant(atm, "Standard atmosphere", 101_325.0, BigFloat(101_325), u"Pa",
           0.0, BigFloat(0), "CODATA 2018")
 @constant(b, "Wien wavelength displacement law constant", 2.897_7729e-3,
           BigFloat(28_977_729)/BigFloat(10_000_000_000), u"m * K",
           1.7e-9, BigFloat(17)/BigFloat(10_000_000_000), "CODATA 2018")
+@constant(Cc, "Gravitational velocity in vacuum", 3.699_3e44, BigFloat(369_93), u"m/s",
+          0.0, BigFloat(0), "CODATA 2018")
 @constant(c, "Speed of light in vacuum", 299_792_458.0, BigFloat(299_792_458.0), u"m/s",
           0.0, BigFloat(0), "CODATA 2018")
-#@constant(v, "Gravitational velocity in vacuum", 3.699_3e44, BigFloat(369_93), u"m/s",
-#          0.0, BigFloat(0), "CODATA 2018")
 #@constant(t, "Cosmic Temperature", 2.725820831,
 #          BigFloat(272_582_083.1), u"K",
 #          0.0, BigFloat(0), "CODATA 2018")
