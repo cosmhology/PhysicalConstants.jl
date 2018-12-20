4713104f03fc4105346ed2dd2b77431239464df6
Laurent G. OSSBSS AMIS UCC <lgueroult@hotmail.com>
2018-12-20 09:29:46 +0100

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,12 @@
+# using Base.Test
+#include("../src/RunTests.jl")
+
 using PhysicalConstant, Measurements, Unitful
-using Test
+if VERSION < v"0.7.0-DEV.2005"
+    using Base.Test
+else
+    using Test
+end
 
 using PhysicalConstant.CODATA2019
 
